054e7da81b2ffa573e163e45b48f1609ab3f86ba
davidjhammaker <davidjhammaker@gmail.com>
2020-11-05 19:30:10 -0500

    Create app, update settings, and reformat

---
+++ votepredictapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VotepredictappConfig(AppConfig):
+    name = "votepredictapp"
--- votepredictbackend/asgi.py
+++ votepredictbackend/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'votepredictbackend.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "votepredictbackend.settings")
 
 application = get_asgi_application()
