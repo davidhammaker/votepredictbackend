ba831b38d5f2a81db1b32211bc63f3f6fd730ee2
davidjhammaker <davidjhammaker@gmail.com>
2020-12-18 22:26:08 -0500

    Return Answers' IDs and contents with Questions

--- votepredictapp/serializers.py
+++ votepredictapp/serializers.py
@@ -4,9 +4,16 @@ from .models import Answer, Prediction, Question, Reply, Vote
 
 
 class QuestionSerializer(serializers.ModelSerializer):
+    answers = serializers.SerializerMethodField()
+
     class Meta:
         fields = ["id", "start_date", "end_date", "content", "answers"]
         model = Question
+
+    def get_answers(self, obj):
+        return [
+            {"id": answer.id, "content": answer.content} for answer in obj.answers.all()
+        ]
 
 
 class AnswerSerializer(serializers.ModelSerializer):
