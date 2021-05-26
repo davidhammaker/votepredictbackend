a471280aca480ca3c3cd02966d7239de36faf7dc
davidjhammaker <davidjhammaker@gmail.com>
2021-05-26 17:03:30 -0400

    Add token login

--- votepredictbackend/urls.py
+++ votepredictbackend/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("votepredictapp.urls")),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
