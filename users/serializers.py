4b2da33e07119b28cf0acdf4e648a13971cd718f
David J. Hammaker <davidjhammaker@gmail.com>
2021-09-11 23:30:14 -0400

    Customize email address validation message

--- users/serializers.py
+++ users/serializers.py
@@ -6,7 +6,12 @@ from rest_framework.validators import UniqueValidator
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
         required=True,
-        validators=[UniqueValidator(queryset=User.objects.all())],
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+                message="A user with that email address already exists.",
+            )
+        ],
     )
     password = serializers.CharField(required=True, write_only=True)
 
